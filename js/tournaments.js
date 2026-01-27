d47714b6ba6968c4fa02d6c9825aaf058765b9f7
jbruses <joacobruses@gmail.com>
2026-01-27 00:13:12 -0300

    Commit: Trying to fix dates

--- js/tournaments.js
+++ js/tournaments.js
@@ -178,16 +178,9 @@ async function deleteTournament(id) {
 
 function editTournament(id) {
   const t = tournaments.find((x) => x.id === id);
-  if (!t) {
-      console.error("❌ No se encontró el torneo con ID:", id);
-      return;
-  }
+  if (!t) return;
 
   editingTournamentId = id;
-
-  console.log("Editando:", t.name);
-  console.log("Fecha Inicio (DB):", t.startDate);
-  console.log("Fecha Fin (DB):", t.endDate);
 
   document.getElementById("tournamentName").value = t.name;
   document.getElementById("tournamentTier").value = t.tier;
@@ -205,10 +198,8 @@ function editTournament(id) {
   };
 
   if (startInput._flatpickr) {
-    console.log("Flatpickr detectado en Inicio. Actualizando...");
     startInput._flatpickr.setDate(normalizeDate(t.startDate), true);
   } else {
-    console.warn("Flatpickr NO detectado en input Inicio (Usando value normal)");
     startInput.value = t.startDate;
   }
 
